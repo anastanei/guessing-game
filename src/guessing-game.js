798b6774fe666e7090c1262f645ab35db54881ef
Anastasiia Nikonova <anastaney@gmail.com>
2024-09-19 13:49:58 +0200

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
 
-    setRange(min, max) {
-
+    constructor() {
     }
 
-    guess() {
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
 
+    guess() {  
+        this.mid =  Math.ceil(this.max / 2 + this.min / 2);
+        return this.mid;
     }
 
     lower() {
-
+        this.setRange(this.min, this.mid);
     }
 
     greater() {
-
+        this.setRange(this.mid, this.max);
     }
 }
 
